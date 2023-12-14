56118e87b800d029413194d9b3d16df68ac94038
Jasper Van der Jeugt <m@jaspervdj.be>
2023-12-14 10:46:53 +0100

    add options record with customizable mime types

--- test/Network/Wai/Handler/HalTest.hs
+++ test/Network/Wai/Handler/HalTest.hs
@@ -4,11 +4,13 @@ module Network.Wai.Handler.HalTest where
 
 import AWS.Lambda.Events.ApiGateway.ProxyRequest
 import Data.Aeson (eitherDecodeFileStrict')
-import qualified Data.Text.Lazy.Encoding as T
+import qualified Data.Text as T
+import qualified Data.Text.Lazy.Encoding as TL
 import Data.Void (Void)
 import Network.Wai.Handler.Hal
 import Test.Tasty
 import Test.Tasty.Golden
+import Test.Tasty.HUnit (assertEqual, testCase)
 import Text.Pretty.Simple
 
 test_ConvertProxyRequest :: TestTree
@@ -17,5 +19,24 @@ test_ConvertProxyRequest =
     proxyRequest :: ProxyRequest Void <-
       eitherDecodeFileStrict' "test/data/ProxyRequest.json"
         >>= either fail pure
-    waiRequest <- toWaiRequest mempty 443 proxyRequest
-    pure . T.encodeUtf8 $ pShowNoColor waiRequest
+    waiRequest <- toWaiRequest defaultOptions proxyRequest
+    pure . TL.encodeUtf8 $ pShowNoColor waiRequest
+
+test_DefaultBinaryMimeTypes :: TestTree
+test_DefaultBinaryMimeTypes = testCase "default binary MIME types" $ do
+  assertBinary "text/plain"               False
+  assertBinary "text/html"                False
+  assertBinary "application/json"         False
+  assertBinary "application/xml"          False
+  assertBinary "application/vnd.api+json" False
+  assertBinary "application/vnd.api+xml"  False
+  assertBinary "image/svg+xml"            False
+
+  assertBinary "application/octet-stream" True
+  assertBinary "audio/vorbis"             True
+  assertBinary "image/png"                True
+  where
+    assertBinary mime expected = assertEqual
+      mime
+      (optionsBinaryMimeType defaultOptions (T.pack mime))
+      expected
